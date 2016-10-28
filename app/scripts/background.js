6d59a0b63a45af872318983810c7298abab79c79
Ray-Chou <zhourui@shinetechchina.com>
2016-10-28 13:30:37 +0800

    1. Remove useless files and useless codes. 2. Clean code. 3. Add notify.js and show it when result is null.

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,34 +1,68 @@
 var clickHandler = function(e) {
     chrome.tabs.query({active: true, currentWindow: true}, function (tabs) {
-        getTemplate(tabs);
+        var result = getSearchResult();
+        if (result === undefined || result === null) {
+            chrome.tabs.sendMessage(tabs[0].id, {action: "notification"});
+        } else if (result.length === 1) {
+            getHTMLTemplate(tabs, "dialog_companyInfo");
+        } else if (result.length > 1) {
+            getHTMLTemplate(tabs, "dialog_searchResult");
+        }
     });
 };
 
 chrome.contextMenus.create({
     "title": "Sök i Tellus",
-    "contexts": ["page", "selection", "image", "link"],
+    "contexts": ["page", "selection"],
+    //"contexts": ["page", "selection", "image", "link"],
     "onclick": clickHandler
 });
 
 chrome.extension.onMessage.addListener(
     function(request, sender, sendResponse) {
-        if(request.action==="open_new_tab"){
-            var newUrl="http://www.google.com"
-            chrome.tabs.create({url:newUrl})
+        if (request.action === "open_new_tab") {
+            var newUrl = "http://www.google.com"
+            chrome.tabs.create({url: newUrl})
         }
     }
 );
 
-function getTemplate(tabs) {
+function getSearchResult() {
+    //return null;
+    return [{"title": "Search Result 1", "details": "1. A quick brown fox jump over the lazy dog."}];
+    //return [{"title": "Search Result 1", "details": "1. A quick brown fox jump over the lazy dog."},
+    //    {"title": "Search Result 2", "details": "2. A quick brown fox jump over the lazy dog."},
+    //    {"title": "Search Result 3", "details": "3. A quick brown fox jump over the lazy dog."},
+    //    {"title": "Search Result 4", "details": "4. A quick brown fox jump over the lazy dog."},
+    //    {"title": "Search Result 5", "details": "5. A quick brown fox jump over the lazy dog."}];
+
+    //$.ajax({
+    //    url: "",
+    //    type: "get",
+    //    success: function (result) {
+    //        //return null;
+    //        return [{"title": "Search Result 1", "details": "1. A quick brown fox jump over the lazy dog."}];
+    //        //return [{"title": "Search Result 1", "details": "1. A quick brown fox jump over the lazy dog."},
+    //        //    {"title": "Search Result 2", "details": "2. A quick brown fox jump over the lazy dog."},
+    //        //    {"title": "Search Result 3", "details": "3. A quick brown fox jump over the lazy dog."},
+    //        //    {"title": "Search Result 4", "details": "4. A quick brown fox jump over the lazy dog."},
+    //        //    {"title": "Search Result 5", "details": "5. A quick brown fox jump over the lazy dog."}];
+    //    },
+    //    error: function (err) {
+    //        console.log(err);
+    //    }
+    //})
+};
+
+function getHTMLTemplate(tabs,action) {
     $.ajax({
-        url: "app/views/test.html",
+        url: "app/views/template.html",
         type: "get",
         success: function (result) {
-            chrome.tabs.sendMessage(tabs[0].id, {action: "open_dialog", html: result}, function (response) {
-            });
+            chrome.tabs.sendMessage(tabs[0].id, {action: action, html: result});
         },
-        error: function (res) {
-            console.log(res);
+        error: function (err) {
+            console.log(err);
         }
     })
 };
--- app/scripts/content.js
+++ app/scripts/content.js
@@ -1,17 +1,28 @@
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
-    if (msg.action == 'open_dialog') {
+    if (msg.action === "notification") {
+        $.notify("Nothing!", {
+            className: 'warn',
+            position: 't c',
+            autoHideDelay: 3 * 1000,
+            clickToHide: true
+        });
+    } else {
         var modal = document.createElement('div');
         modal.innerHTML = msg.html;
 
         var popup = $(modal);
         popup.appendTo($("body"));
 
-        $('#MyModal1').modal('show');
+        if (msg.action === "dialog_companyInfo") {
+            $('#ModalCompanyInfo').modal('show');
+        } else if (msg.action === 'dialog_searchResult') {
+            $('#ModalSearchResult').modal('show');
+        }
     }
 });
 
 $(document).ready(function () {
-    $("body").on("click","#btn-open-tellus",function(){
-        chrome.extension.sendMessage({action:'open_new_tab'});
+    $("body").on("click", "#btn-open-tellus", function () {
+        chrome.extension.sendMessage({action: 'open_new_tab'});
     })
 });
